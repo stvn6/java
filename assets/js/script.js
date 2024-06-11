9bc366cc6239a80717ee4123edeac64a7820ffae
Chaiguata 1 <stevenbenjamin.pavez@alumnos.ulagos.cl>
2024-06-11 09:07:00 -0400

    a

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+/*Declara una variable llamada nombre.
+Asigna tu nombre a la variable nombre.
+Declara una variable llamada edad y asígnale tu edad.
+Imprime ambos valores en la consola.*/
+let nombre := "steven";
+let edad := 19;
+console.log(nombre);
+console.log(edad);
+
+/* Declara una variable de tipo número, una de tipo cadena de texto y una de tipo booleano.
+Asigna valores a cada una de estas variables.
+Imprime el tipo de dato de cada variable usando typeof.*/
+let numero : = 20;
+let texto = "chao";
+let booleano = true;
+console.log(numero);
+console.log(texto);
+console.log(booleano);
+
+/* Agrega un comentario de una sola línea y un comentario de varias líneas en el código.
+Declara una variable llamada saludo y asígnale un mensaje de saludo.
+Imprime el mensaje de saludo en la consola.*/
+let saludo = "hola, un saludo";
+console.log(saludo);
+
+/*Declara una constante llamada PI y asígnale el valor 3.14159.
+Intenta cambiar el valor de PI y observa lo que sucede.
+Imprime el valor de PI en la consola. */
+const pi = 3.14159;
+pi = 10;
+console.log(pi);
